4d4b77893ddcb8be3f8dc4c2e5f084df149844ca
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2024-09-07 20:20:22 -0300

    chore: add swagger, class-validator and class-transformer packages

--- taskflow-server/src/main.ts
+++ taskflow-server/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Taskflow')
+    .setDescription('The taskflow API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(5174);
 }
 bootstrap();
