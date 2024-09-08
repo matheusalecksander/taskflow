88ab4e7b766182f1f2f40669e739c166328f476f
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2024-09-08 19:55:16 -0300

    refactor: adjusting the payload mapping

--- taskflow-server/src/_core/auth/jwt.strategy.ts
+++ taskflow-server/src/_core/auth/jwt.strategy.ts
@@ -13,6 +13,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    return {
+      id: payload.sub,
+      name: payload.name,
+      email: payload.email,
+      role: payload.role,
+    };
   }
 }
